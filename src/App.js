972017c1903b936cce2681a1b13b708f9b0bbe45
val089 <kamilszerlag@gmail.com>
2020-12-06 19:22:45 +0100

    version 1

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './App.scss';
+
+import Title from "./Components/Title";
+import Calculator from "./Components/Calculator";
+
+function App() {
+  return (
+    <div className="App">
+      <Title />
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
